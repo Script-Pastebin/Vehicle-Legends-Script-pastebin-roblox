d7c45202ea957c29941c94aa52a66b4515ee4bb3
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:48 +0100

    commit zero

---
+++ Vehicle Legends Script pastebin roblox Vehicle Legends AutoFarm .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/NY8M7rYV"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							GUI, AUTO FARM, SAFE ZONE
+						
+]]
